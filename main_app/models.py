8c8f05901210eafe6e74e445e89b67041eeb2215
Den104Ten <dan_eltsov@mail.ru>
2023-08-14 17:58:46 +0300

    Добавил создание постов их открытие и view of posts

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.urls import reverse
+
 
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
